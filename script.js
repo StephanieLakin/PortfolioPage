a21275475c6d4bee2ab9c4f6f501e76b7d776c76
Stephanie Lakin <stephanielakin13@outlook.com>
2020-06-14 13:56:09 -0400

    adding projects; restyling to change theme

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ $(function () {
 
 // Adding animations to the sections
 $('section').addClass('wow fadeIn');
-
 // Animations Init
 new WOW().init();
+
+
+// Adding animations to the buttons
+Waves.attach('.btn', ['waves-effect']);
+
+// trying to eliminate scrollbars
+$.each($('*'), function () {
+  if ($(this).width() > $('body').width()) {
+    console.log('Wide Element: ', $(this), 'Width: ', $(this).width());
+  }
+});

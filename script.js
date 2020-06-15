7e81ce1446b21d2bb59df9bac00a6b6c74d9a3d3
Stephanie Lakin <stephanielakin13@outlook.com>
2020-06-14 21:48:45 -0400

    theme and styling changes; adding  image gallery

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ $('section').addClass('wow fadeIn');
 // Animations Init
 new WOW().init();
 
-
 // Adding animations to the buttons
 Waves.attach('.btn', ['waves-effect']);
 
@@ -16,4 +15,16 @@ $.each($('*'), function () {
   if ($(this).width() > $('body').width()) {
     console.log('Wide Element: ', $(this), 'Width: ', $(this).width());
   }
+});
+
+// MDB Lightbox Init
+// $(function () {
+//   $('#mdb-lightbox-ui').load('mdb-addons/mdb-lightbox-ui.html');
+// });
+
+// Gallery
+
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
 });
